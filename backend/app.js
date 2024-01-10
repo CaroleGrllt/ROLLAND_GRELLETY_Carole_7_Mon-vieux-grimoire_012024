7892ece23e942f68d458593c7770c365470a6886
Carole ROLLAND GRELLETY <carole.grellety@gmail.com>
2024-01-10 22:05:44 +0100

    Importation front + configuration back

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bookRoutes = require('./routes/book');
+const userRoutes = require('./routes/user')
+
+const app = express();
+
+mongoose.connect('mongodb+srv://root:root@mon-vieux-grimoire.ccrkhxa.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+  
+app.use(express.json());
+
+// app.use((req, res) => {
+//    res.json({ message: 'Votre requête a bien été reçue le 5 janvier 2023 !' }); 
+// });
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  app.use('/api/books', bookRoutes);
+  app.use('/api/auth', userRoutes);
+
+module.exports = app;
---
+++ backend/routes/book.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const bookCtrl = require('../controllers/book')
+
+router.post('/', auth, bookCtrl.createBook);
+router.get('/', bookCtrl.getAllBooks);
+router.get('/:id', bookCtrl.getOneBook);
+router.put('/:id', auth, bookCtrl.modifyBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
+
+module.exports = router;
